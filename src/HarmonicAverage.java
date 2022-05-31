d4fe745454b08d3e03484e31cdec985f49d1a203
Burak Özer <burakozer539@gmail.com>
2022-05-31 23:58:52 +0300

    arrays

--- src/HarmonicAverage.java
+++ src/HarmonicAverage.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class HarmonicAverage {
+public class HarmonicAverage {
+
+    public static void main(String[] args) {
+
+        int[] list  = {2,5,12,34,23,124};
+        double harmonicSeries = 0.0;
+
+
+        for(int i = 0;i<list.length;i++){
+
+            harmonicSeries += (1.0/list[i]);
+        }
+
+        double harmonicAverage = harmonicSeries/list.length;
+        System.out.println("Harmonic Average: " + harmonicAverage);
+
+    }
+
 }
