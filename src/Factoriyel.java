5ba59a8cc0186d82adfc7a732b6f0debe5e50eaf
Burak Özer <burakozer539@gmail.com>
2022-06-17 21:18:33 +0300

    sayi tahmin

--- src/Factoriyel.java
+++ src/Factoriyel.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Factoriyel {
+import java.util.Scanner;
+public class Factoriyel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+        System.out.print("Sayi giriniz: ");
+        int n = sc.nextInt();
+        double fuc = 1;
+
+        if (n < 0) {
+            System.out.println("Pozitif bir sayi giriniz");
+        }
+
+        else {
+
+            if (n == 0) {
+                fuc = 1;
+            }
+
+            else {
+                for (int i = 1; i <= n; i++) {
+
+                    fuc *= i;
+                }
+            }
+        }
+        System.out.println(n + " 'n faktoriyeli: " + fuc);
+    }
+
 }
--- src/MukemmelSayi.java
+++ src/MukemmelSayi.java
@@ -1,5 +1,25 @@
+
+import java.util.Scanner;
 public class MukemmelSayi {
     public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.print("Enter a number: ");
+            int n = scanner.nextInt();
+            int sayac = 1;
+            int ms = 0;
+            while (n > sayac) {
+                if (n % sayac == 0) {
+                    ms += sayac;
+                }
+                sayac++;
+
+            }
+            if (ms == n) {
+                System.out.println(n + " mukemmel sayidir");
+            } else {
+                System.out.println(n + " mukemmel sayi degildir");
+            }
+        }
     }
-}
+
