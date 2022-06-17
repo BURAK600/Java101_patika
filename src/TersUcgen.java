e51ec358f170081bd6d5e4d75458e4307607c676
Burak Özer <burakozer539@gmail.com>
2022-06-17 21:41:05 +0300

    ter ucgen

---
+++ src/TersUcgen.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("bir sayi giriniz: ");
+
+        int n = scanner.nextInt();
+        int k =0;
+        while(2*n!=0){
+            for(int j = 0;j<k;j++){
+                System.out.print(" ");
+            }
+            for(int i = 0;i<2*n-1;i++){
+
+                System.out.print("*");
+            }
+
+            for(int j = 0;j<k;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+            k++;
+
+            n--;
+        }
+    }
+}
