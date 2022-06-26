c65b353465638dec2b195c92e49386fecef4f093
Burak Özer <burakozer539@gmail.com>
2022-06-26 22:51:57 +0300

    transpoze

---
+++ src/MatrixTranspose.java
@@ -0,0 +1,25 @@
+public class MatrixTranspose {
+
+    public static void main(String[] args) {
+
+        int[][] matris = {{1,2,3},{4,5,6}};
+
+        for (int i =0;i< matris[1].length;i++){
+            for (int j = 0;j<matris.length;j++ ) {
+
+
+                System.out.print(matris[j][i] + " ");
+
+            }
+            System.out.println();
+
+
+
+
+
+        }
+
+
+
+    }
+}
